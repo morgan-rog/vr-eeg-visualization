e62d0b579c4db8a940d944128b784aaed3167d3a
Morgan Rogers <morgan.rog@yahoo.com>
2022-12-14 20:52:12 -0600

    initial files

---
+++ Assets/Scripts/LogReader.cs
@@ -0,0 +1,144 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Globalization;
+using System.Collections.Generic;
+using System.IO;
+
+public class LogReader : MonoBehaviour {
+
+
+public bool readLog;
+
+bx3_main bx3; 
+StreamReader reader;
+public List <string> lines;
+public List <float> times;
+
+string message;
+
+int index;
+
+float counter;
+    // Use this for initialization
+
+void Awake(){
+
+        bx3 = GameObject.Find("brainX3").GetComponent<bx3_main>();
+        //Debug.Log(exp);
+        //Debug.Log(Application.dataPath);
+
+        readLogfromText();
+
+
+    }
+
+void Start () {
+
+
+}
+
+
+    public void readLogfromText()
+    {
+        FileInfo txt = new FileInfo(Application.dataPath + "/MNI/" + "example_coordinates.csv");
+        lines = new List<string>();
+        if (txt != null)
+        {
+            reader = txt.OpenText();
+        }
+        string xT = reader.ReadToEnd();
+        //Debug.Log (xT);
+        char[] lineSeparator = new char[] { '\n' };
+        string[] linesS = xT.Split(lineSeparator);
+        for (int i = 0; i <= linesS.Length - 1; i++)
+        {
+            lines.Add(linesS[i]);
+            //Debug.Log (linesS[i]);
+        }
+
+        char[] dataSeparator = new char[] { ';' };
+        char[] colon = new char[] { ',' };
+
+        foreach (string line in lines)
+        {
+            //Debug.Log(line);
+            if (line != null)
+            {
+                if (line == "#Starting log sesion")
+                    continue;
+                if (line == "#Ending log sesion")
+                    break;
+                string[] data = line.Split(dataSeparator);
+
+                //Debug.Log(data.Length);
+
+                if (data.Length == 4 && data[1] != "RAS-Slicer")
+                {
+                    char[] dataSeparator1 = new char[] { ' ' };
+                    string[] datainrow = data[2].Split(dataSeparator1);
+                    //Debug.Log(datainrow.Length);
+                    int count = 0;
+                    string[] realdatainrow = new string[3];
+                    for (int i = 0; i < datainrow.Length; i++)
+                    {
+                        if (datainrow[i] != "[" && datainrow[i] != " " && datainrow[i] != "]" && datainrow[i] != "")
+                        {
+                            //Debug.Log(datainrow[i]);
+                            realdatainrow[count] = datainrow[i];
+                            count++;
+                            //Debug.Log(realdatainrow[count]);
+
+                        }
+
+                    }
+                    float mniX = float.Parse(realdatainrow[0].Replace("[", ""));
+                    float mniY = float.Parse(realdatainrow[1]);
+                    //Debug.Log(datainrow[2]);
+                    float mniZ = float.Parse(realdatainrow[2].Replace("]", ""));
+                    //Debug.Log(mniZ);
+
+                    bx3.MNI.Add (new Vector3(mniX, mniY, mniZ));
+
+                }
+            }
+
+
+        }
+    }
+    }
+
+
+        
+
+
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assets/Scripts/bx3_main.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bx3_main : MonoBehaviour
+{
+
+
+    GameObject participant;
+    GameObject l_hem;
+    GameObject r_hem;
+
+    Vector3[] vertices_l;
+    Mesh mesh_l;
+    Vector3[] vertices_r;
+    Mesh mesh_r;
+    public List<Vector3> MNI;
+
+    //public float[] distances;
+    List<GameObject> col_vertices_l;
+    List<GameObject> col_vertices_r;
+
+    Color[] colors4surfP; 
+
+    GameObject locator;
+    int maxElec;
+    bool once;
+    private void Awake()
+    {
+     
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log ("hello world");
+        //log.LogMessage("hello world");
+
+        l_hem = GameObject.Find("Left-Cerebral-Cortex");
+        r_hem = GameObject.Find("Right-Cerebral-Cortex");
+
+        mesh_l = l_hem.GetComponent<MeshFilter>().mesh;
+        vertices_l = mesh_l.vertices;
+
+        mesh_r = r_hem.GetComponent<MeshFilter>().mesh;
+        vertices_r = mesh_r.vertices;
+
+        createColliders();
+
+        Debug.Log(vertices_l.Length);
+
+        createElectrodes();
+
+        maxElec = 13;
+        colors4surfP = new Color[maxElec];
+
+        colors4surfP[0] = Color.black;
+        colors4surfP[1] = new Color(0.25f, 0, 0);
+        colors4surfP[2] = new Color(0.5f, 0, 0);
+        colors4surfP[3] = new Color(0.75f, 0f, 0);
+        colors4surfP[4] = new Color(1, 0f, 0);
+        colors4surfP[5] = new Color(1f, .25f, 0);
+        colors4surfP[6] = new Color(1, 0.5f, 0);
+        colors4surfP[7] = new Color(1, 0.75f, 0);
+        colors4surfP[8] = new Color(1, 1, 0); 
+        colors4surfP[9] = new Color(1, 1, 0.2f); 
+        colors4surfP[10] = new Color(1, 1, 0.4f);
+        colors4surfP[11] = new Color(1, 1, 0.6f);
+        colors4surfP[12] = new Color(1f, 1f, 0.8f);
+
+
+
+
+    }
+
+    void Update()
+    {
+    
+        // this should be in the start function but info from colliders is not updated
+
+        Color[] colors_l = new Color[vertices_l.Length];
+        for (int i = 0; i < col_vertices_l.Count; i++)
+        {
+            if (col_vertices_l[i].GetComponent<col_component>().ncloseElectrodes >= maxElec)
+            {
+                colors_l[i] = new Color(1, 1, 1f);
+            }
+
+            for (int j = 0; j < colors4surfP.Length; j++)
+            {
+                if (col_vertices_l[i].GetComponent<col_component>().ncloseElectrodes == j)
+                    colors_l[i] = colors4surfP[j];
+            }
+
+
+        }
+        mesh_l.colors = colors_l;
+
+
+
+
+
+    }
+
+    private int getMaxElec()
+    {
+        int max = 0;
+        for (int i = 0; i < col_vertices_l.Count; i++)
+        {
+            int num = col_vertices_l[i].GetComponent<col_component>().ncloseElectrodes;
+            //Debug.Log(num);
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+        return max;
+    }
+
+
+    void createColliders()
+    {
+        col_vertices_l = new List<GameObject>();
+        GameObject colliders_vL = new GameObject("Vertex_colliders_L"); 
+        foreach (Vector3 v3 in vertices_l)
+        {
+            GameObject go = new GameObject("vertex_L");
+            go.transform.position = v3;
+            go.transform.parent = colliders_vL.transform;
+            go.AddComponent<SphereCollider>();
+            go.AddComponent<col_component>();
+            col_vertices_l.Add(go);
+        }
+
+
+    }
+
+    void createElectrodes()
+    {
+
+        GameObject goP = new GameObject("Electrodes");
+        GameObject s = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        s.AddComponent<Rigidbody>().isKinematic = true;
+        s.GetComponent<SphereCollider>().isTrigger = true;
+        s.GetComponent<SphereCollider>().radius = 12.5f; // original radius is doubled
+
+        for (int i = 0; i < MNI.Count; i++)
+        {
+            GameObject go = Instantiate(s);
+            go.transform.parent = goP.transform;
+            go.transform.position = MNI[i];
+            //go.transform.localScale = new Vector3(2, 2, 2);
+            go.GetComponent<MeshRenderer>().material = new Material(Shader.Find("Specular"));
+            go.GetComponent<MeshRenderer>().material.color = Color.red;
+        }
+
+        goP.transform.eulerAngles = new Vector3(-90, 0, 0);
+        Destroy(s);
+
+    }
+
+
+
+}
